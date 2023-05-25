d875a17eb30c0dcf9f241e1b60fcee84ebbe247f
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-05-25 16:20:24 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havenese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
